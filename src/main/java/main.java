6d4018101f5d805d7c47347ed92e7db4914de837
thevoyagertwo <thevoyagertwo@protonmail.com>
2021-03-23 12:02:05 +0000

    cleaned comments up of main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,38 +5,6 @@ import Utility.ReadFile;
 import java.util.Arrays;
 
 
-//public class main {
-//
-//    public static void main(String[] args) {
-//
-//        BodyVariables help = BodyVariables.TestBody();
-//
-//
-//
-//        System.out.println(help.getName());
-//        help.setName("try");
-//        System.out.println(help.getName());
-//
-//
-//        BodyVariablesArray twoBody = new BodyVariablesArray();
-//
-//
-//        System.out.println(twoBody.bodies.get(0).getMass() + " getMass");
-//
-//        System.out.println(twoBody.calculateDistance(0 , 1));
-//
-//
-//        // should be opposite sign as they calculate 0's force on 1 then vice versa
-//        System.out.println(Arrays.toString(twoBody.calculateAttractionTwoBody(0, 1)));
-//        System.out.println(Arrays.toString(twoBody.calculateAttractionTwoBody(1, 0)));
-//
-//        String[] testRead = ReadFile.fileReader("voyager");
-//        System.out.println(testRead[5]);
-//
-//    }
-//
-//}
-
 public class main {
 
     public static void main(String[] args) {
